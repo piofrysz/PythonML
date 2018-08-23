0829cd62388e7192b599b2408e391ca503f0c633
piofrysz <piofrysz@gmail.com>
2018-08-24 00:34:19 +0200

    PyCharm TEST

---
+++ Learning/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+testX = np.array([1,5,2,6,3,4])
+testY = np.array([1,3,5,7,2,1])
+
+plt.scatter(testX, testY)
+plt.show()
+
+class Test():
+    def __init__(self):
+        pass
+
+print("TEST")
+
